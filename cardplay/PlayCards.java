4fb157db95e868e343561ac0a2f9a06b5e2e36f8
hippyTed <ted@home-net>
2013-05-20 13:18:46 +0100

    Small cleanup

--- cardplay/PlayCards.java
+++ cardplay/PlayCards.java
@@ -16,12 +16,9 @@ public class PlayCards {
 		for (int h = 0; h < PLAYERS; h++) 
 			hand[h] = new Hand(cardsEach);
 		
-		for (int i = 0; i < cardsEach; i++) {
-			for (int player = 0; player < PLAYERS; player++) {
-				Card c = cards.deal();
-				hand[player].addCard(c);
-			}
-		}
+		for (int i = 0; i < cardsEach; i++)
+			for (int player = 0; player < PLAYERS; player++)
+				hand[player].addCard(cards.deal());
 		
 		for (int player = 0; player < PLAYERS; player++) {
 			System.out.println("\nPlayer" + (player+1) 
