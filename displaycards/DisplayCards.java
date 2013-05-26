86879c28bf508e08fe1b03d54c2cb462865addd6
hippyTed <ted@home-net>
2013-05-26 20:43:28 +0100

    Extensive changes that get it working

--- displaycards/DisplayCards.java
+++ displaycards/DisplayCards.java
@@ -1,15 +1,14 @@
 package displaycards;
 
-import java.awt.EventQueue;
 import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class DisplayCards {
 	
@@ -18,38 +17,37 @@ public class DisplayCards {
 		// Event dispatch thread
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				JFrame frame = new MainFrame();
-				frame.setTitle("DisplayCards");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
+				createMainWindow();
 			}
 		});			
 	
 	}
+	
+	private static void createMainWindow() {
+		JFrame frame = new JFrame("DisplayCards");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		frame.add(new MainPanel());
+		
+		frame.pack();
+		frame.setVisible(true);		
+	}
 }
 	
-class MainFrame extends JFrame {
+class MainPanel extends JPanel {
 	static final long serialVersionUID = 1L;
-	private static final int FRAME_WIDTH = 900;
-	private static final int FRAME_HEIGHT = 300;
+	private static final int FRAME_WIDTH = 850;
+	private static final int FRAME_HEIGHT = 200;
 	
-	public MainFrame() {
-		PlayCards game = new PlayCards();
-		
+	public MainPanel() {
+
+		PlayCards game = new PlayCards();		
 		game.dealCards(PlayCards.PLAYERS);
 		
-		for(int player = 1; player <= PlayCards.PLAYERS; player++) {
+		for(int player = 1; player <= PlayCards.PLAYERS; player++)
 			add(new HandComponent(player));
-			System.out.println("Added Player" + player + "'s hand component!");
-		}
 
-		JPanel buttonPanel = new JPanel();
-
-		buttonPanel.add(new QuitButton("Quit"));
-		add(buttonPanel);
-		System.out.println("Added Quit button JPanel!");
-		
-		pack();
+		add(new QuitButton("Quit"));
 	}
 
 	public Dimension getPreferredSize() {
@@ -57,26 +55,17 @@ class MainFrame extends JFrame {
 	}
 }
 
-class HandComponent extends JComponent {
+class HandComponent extends JTextArea {
 	static final long serialVersionUID = 10L;
 	private static final int HAND_WIDTH = 200;
-	private static final int HAND_HEIGHT = 200;
-	int playerIndex;
+	private static final int HAND_HEIGHT = 130;
 	
 	public HandComponent(int playerNumber) {
-		System.out.println("Making Player" + playerNumber + "'s JComponent!");
-		playerIndex = playerNumber - 1;
+		append("Player " + playerNumber);
+		append(PlayCards.hand[playerNumber - 1].getHand());
+		setEditable(false);
 	}
-	
-	// called automatically, on expose events etc
-	public void paintComponent(Graphics g) {		
-		int x = playerIndex*(HAND_WIDTH + 20) + 20;
-		int y = 20;
-		final Hand h = PlayCards.hand[playerIndex];
-		g.drawString(h.getHand(), x, y);
-		System.out.println("Drawn Player" + (playerIndex+1) + "'s Component!");
-	}
-	
+		
 	public Dimension getPreferredSize() {
 		return new Dimension(HAND_WIDTH, HAND_HEIGHT);
 	}
@@ -88,16 +77,14 @@ class QuitButton extends JButton {
 	private static final int BUTTON_HEIGHT = 30;
 
 	QuitButton(String name) {
-		System.out.println("Making JButton(" + name + ")!");
-		JButton button = new JButton(name);
-	
-		button.addActionListener(new ActionListener() {
+		super(name);
+		addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				System.out.println("Button pressed!");
 				System.exit(0);
 			}
 		});
 	}
+	
 	public Dimension getPreferredSize() {
 		return new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT);
 	}
